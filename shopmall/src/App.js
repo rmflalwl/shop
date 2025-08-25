a3b648ee191030610c1ce23dee27b45cf6f55369
DESKTOP-5L8JNM3\LINE <rmflalwl@gmail.com>
2025-08-25 15:52:19 +0900

    App.js 수정

--- shopmall/src/App.js
+++ shopmall/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+     <button type="button" class="btn btn-primary">Primary</button>
     </div>
   );
 }
